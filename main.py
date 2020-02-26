00000ac88e0b04eea491e84eee5077c0d8bdd424
binarioGH <lacuentafalsadediegojonas@gmail.com>
2020-02-25 21:57:42 -0800

    bugs solved, sutff improved

--- main.py
+++ main.py
@@ -3,18 +3,38 @@ from tkinter import *
 from json import loads
 with open("atoms.json", "r") as f:
 	ATOMS = loads(f.read())
+with open("symbols.json", "r") as f:
+	SYMBOLS = loads(f.read())
 
-def calculate(element1, element2):
-	element1 = element1.lower()
-	element2 = element2.lower()
+def convert(element):
+	if element in SYMBOLS:
+		return SYMBOLS[element]
+	return "-*-*-*-*-"
+
+
+def calculate(element1, element2, show_process=False):
+	element1 = element1.lower().strip()
+	element2 = element2.lower().strip()
+	errcounter = 0
 	if element1 not in ATOMS or element2 not in ATOMS:
-		return "Check the spelling of the elements."
-	difference = ATOMS[element1] - ATOMS[element2]
+		element1 = convert(element1.title())
+		element2 = convert(element2.title())
+		if element1 not in ATOMS or element2 not in ATOMS:
+			return "Check the spelling of the elements."
+	electro_negativity1 = ATOMS[element1]
+	electro_negativity2 = ATOMS[element2]
+	difference = electro_negativity1 - electro_negativity2
 	if difference < 0:
-		difference *= -1
-	if difference >= 0.4:
+		difference *= -1	
+	if show_process:
+		print("{} -> {}".format(element1, electro_negativity1))
+		print("{} -> {}".format(element2, electro_negativity2))
+		print("Difference: {}".format(difference))
+	
+	
+	if difference <= 0.4:
 		return "Non-polar"
-	elif difference >= 1.9:
+	elif difference <= 1.9:
 		return "Polar"
 	else:
 		return "Ionic"
